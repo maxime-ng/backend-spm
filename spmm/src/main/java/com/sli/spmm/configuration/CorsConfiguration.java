5b75c3b5c06316aac54ab388c4bc631fa316f30f
Maximeng <83396993+maxime-ng@users.noreply.github.com>
2022-07-29 11:02:12 +0200

    back

---
+++ spmm/src/main/java/com/sli/spmm/configuration/CorsConfiguration.java
@@ -0,0 +1,51 @@
+<<<<<<< HEAD
+package com.sli.spmm.configuration;
+
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("Origin", "Accept", "Content-Type", "Authorization")
+                .exposedHeaders("Content-Disposition", "Authorization", "Link", "X-Total-Count");
+    }
+    
+    
+}
+=======
+package com.sli.spmm.configuration;
+
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("Origin", "Accept", "Content-Type", "Authorization")
+                .exposedHeaders("Content-Disposition", "Authorization", "Link", "X-Total-Count");
+    }
+    
+    
+}
+>>>>>>> a8af1347c237e6049abd62e3dbc04ce6b8eabb1e
